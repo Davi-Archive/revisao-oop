d615dc901a6ddeaca8c1507d7e255186a95e3f27
davi38 <davi4alves@gmail.com>
2022-10-30 20:38:23 -0300

    feat: new encapsulation

---
+++ oop6/src/oop6/ControleMain.java
@@ -0,0 +1,13 @@
+package oop6;
+
+public class ControleMain {
+
+	public static void main(String[] args) {
+		ControleRemoto c = new ControleRemoto();
+		c.ligar();
+		c.maisVolume();
+		c.abrirMenu();
+		
+	}
+
+}
